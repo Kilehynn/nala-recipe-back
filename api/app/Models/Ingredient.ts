d16494a5fec792c0496b148f27962c887dee7750
mexane.delcroix <mexane.delcroix@epita.fr>
2021-08-15 17:14:44 +0200

    fix[database] : Save some forgotten files

--- api/app/Models/Ingredient.ts
+++ api/app/Models/Ingredient.ts
@@ -1,9 +1,20 @@
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+import { BaseModel, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm';
+import Recipe from "App/Models/Recipe";
 import { DateTime } from 'luxon';
 
 export default class Ingredient extends BaseModel {
   @column({ isPrimary : true })
   public id: number;
+
+
+  @manyToMany(() => Recipe, {
+    localKey : 'id',
+    relatedKey : 'id',
+    pivotColumns : ['quantity'],
+    pivotTimestamps : true,
+  })
+  public recipes: ManyToMany<typeof Recipe>;
+
 
   @column.dateTime({ autoCreate : true })
   public createdAt: DateTime;
--- api/app/Models/User.ts
+++ api/app/Models/User.ts
@@ -1,8 +1,17 @@
 import Hash from "@ioc:Adonis/Core/Hash";
-import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm';
+import { BaseModel, beforeSave, column, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm';
+import Recipe from "App/Models/Recipe";
 import { DateTime } from 'luxon';
 
 export default class User extends BaseModel {
+
+  @manyToMany(() => Recipe,
+    {
+      localKey : 'id',
+      relatedKey : 'id',
+      pivotTimestamps : { createdAt : true, updatedAt : false },
+    })
+  public favorites: ManyToMany<typeof Recipe>;
 
   @column({ isPrimary : true })
   public id: number;
